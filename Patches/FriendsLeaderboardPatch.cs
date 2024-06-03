4e9d9e31efbf06392880c428bdde9525666ac56b
taypexx <taypexoffcl@gmail.com>
2024-06-04 01:52:10 +0300

    fixed updating

--- Patches/FriendsLeaderboardPatch.cs
+++ Patches/FriendsLeaderboardPatch.cs
@@ -13,12 +13,12 @@ namespace FriendsLeaderboard.Patches
     [HarmonyPatch(typeof(PnlRank), nameof(PnlRank.UIRefresh))]
     public class FriendsLeaderboardPatch
     {
+
         private static void Postfix()
         {
-
-            string[] friendsNames = Save.Data.FriendsNames;
-
+            Color defaultColor = new Color(0.8353f, 0.7373f, 1, 0.6f);
             Color highlightColor = Save.Data.HighlightColor;
+            string[] friendsNames = Save.Data.FriendsNames;
 
             var rankCells = GameObject.Find("UI/Standerd/PnlPreparation/Panels/PnlRankLocalization/Pc/PnlRank/ScvRank/Viewport/Content");
 
@@ -52,6 +52,18 @@ namespace FriendsLeaderboard.Patches
 
                     var accval = cell.transform.Find("TxtAccuracyValueS");
                     accval.gameObject.GetComponent<UnityEngine.UI.Text>().color = highlightColor;
+                } else
+                {
+                    nameval.gameObject.GetComponent<UnityEngine.UI.Text>().color = defaultColor;
+
+                    var rankval = cell.transform.Find("TxtRankValueS");
+                    rankval.gameObject.GetComponent<UnityEngine.UI.Text>().color = defaultColor;
+
+                    var scoreval = cell.transform.Find("TxtScoreValueS");
+                    scoreval.gameObject.GetComponent<UnityEngine.UI.Text>().color = defaultColor;
+
+                    var accval = cell.transform.Find("TxtAccuracyValueS");
+                    accval.gameObject.GetComponent<UnityEngine.UI.Text>().color = defaultColor;
                 }
 
             } 
